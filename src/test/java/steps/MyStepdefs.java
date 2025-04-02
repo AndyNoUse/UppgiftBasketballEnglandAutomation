2d37aa5ead026ba32691b2652d2a2178622c219f
lonil <lonil005@gmail.com>
2025-04-02 14:14:31 +0200

    initial add

--- src/test/java/steps/MyStepdefs.java
+++ src/test/java/steps/MyStepdefs.java
@@ -1,4 +1,82 @@
 package steps;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MyStepdefs {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+
+    @Given("I am on basketballengland.co.uk")
+    public void iAmOnBasketballenglandCoUk() {
+
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        driver.get("https://membership.basketballengland.co.uk/NewSupporterAccount");
+    }
+
+    @When("I fill in the correct member details")
+    public void iFillInTheCorrectMemberDetails() {
+        String testEmail = "test4@gmail.com"; // Rewrite to a dynamic email
+        String testPassword = "password";
+        driver.findElement(By.cssSelector("input#dp")).sendKeys("07/01/1999"); //Date of Birth DD/MM/YYYY
+        driver.findElement(By.cssSelector("input#member_firstname")).sendKeys("Urban"); //First Name
+        driver.findElement(By.cssSelector("input#member_lastname")).sendKeys("Test"); //Last name
+        driver.findElement(By.cssSelector("input#member_emailaddress")).sendKeys(testEmail); //email
+        driver.findElement(By.cssSelector("input#member_confirmemailaddress")).sendKeys(testEmail); //confirm email
+        driver.findElement(By.cssSelector("#signupunlicenced_password")).sendKeys(testPassword); //Password
+        driver.findElement(By.cssSelector("#signupunlicenced_confirmpassword")).sendKeys(testPassword); //Confirm password
+        //wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".redbox")));
+        //wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector(".inc"))));
+        driver.findElement(By.cssSelector("label[for='sign_up_25'] span[class='box']")).click(); //I have read, understood Terms and Conditions
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector(".inc"))));
+        driver.findElement(By.cssSelector("label[for='sign_up_26'] span[class='box']")).click(); //I am aged over 18
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector("label[for='sign_up_26'] span.check"))));
+        driver.findElement(By.cssSelector("label[for='fanmembersignup_agreetocodeofethicsandconduct'] span[class='box']")).click(); //I have read, Understood Code of conduct
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector(".inc"))));
+        driver.findElement(By.cssSelector(".btn")).click();
+        //input[value='CONFIRM AND JOIN']
+        //label[for='sign_up_26'] span[class='check']
+        //label[for='sign_up_26'] span.check
+    }
+
+    //    @Then("I successfully become a member")
+//    public void iSuccessfullyBecomeAMember() {
+//        assertEquals("THANK YOU FOR CREATING AN ACCOUNT WITH BASKETBALL ENGLAND", driver.findElement(By.cssSelector("h2.gray")).getText());
+//    }
+    @Then("I successfully become a member")
+    public void iSuccessfullyBecomeAMember() {
+        //wait.until(ExpectedConditions.visi)
+        String expected = "THANK YOU FOR CREATING AN ACCOUNT WITH BASKETBALL ENGLAND";
+        String actual = driver.findElement(By.cssSelector("h2.gray")).getText();
+        assertEquals(expected, actual);
+        System.out.println("DET GICK!!!");
+    }
+
+
+    @When("I remove {string}")
+    public void iRemove(String elementTobeCleared) {
+        driver.findElement(By.cssSelector("input#member_" + elementTobeCleared)).clear(); //Last name
+
+    }
+
+    @Then("I fail to become a new member")
+    public void iFailToBecomeANewMember() {
+    }
+
+    @Given("I am on basketballengland.co.uk on {}")
+    public void iAmOnBasketballenglandCoUkOn(String arg0) {
+    }
 }
---
+++ src/test/resources/features/basketBallClub.feature
@@ -0,0 +1,53 @@
+Feature:Test of regristration of users
+
+  Scenario: Add new user and everything works as expected
+    Given I am on basketballengland.co.uk
+    When I fill in the correct member details
+    Then I successfully become a member
+
+  #Scenario: Add new user but lastname is missing
+    Given I am on basketballengland.co.uk
+    When I fill in the correct member details
+    And  I remove "lastname"
+    Then I fail to become a new member
+
+  #Scenario: Add new user but password does not match
+    Given I am on basketballengland.co.uk
+
+
+ # Scenario: Add new user Terms and conditions not accepted
+    Given I am on basketballengland.co.uk
+
+
+  Scenario Outline: Add new user and everything works as expected on different browsers
+    Given I am on basketballengland.co.uk on <browser>
+    When I fill in the correct member details
+    Then I successfully become a member
+    Examples:
+      | browser |
+      | chrome  |
+      | firefox |
+      | edge    |
+
+    #På
+    #https://membership.basketballengland.co.uk/NewSupporterAccount
+    # sig som användare
+    # kan man registrera
+    #❖ Skriv testfall som testar denna
+    #registreringsfunktion genom automatisering
+    #via Cucumber och Selenium
+    # ❖ Testfallen skall täcka kraven på kommande
+    #slides
+
+    # ❖ Skapa användare – allt går som förväntat och ett konto skapas X
+    # ❖ Skapa användare – efternamn saknas
+    # ❖ Skapa användare – lösenord matchar inte
+    # ❖ Skapa användare – terms and conditions är inte godkänt
+
+    # ❖ Verifiering skall ske på varje scenario med minst en Junit assert
+    # ❖ Feature-filen skall vara skapad enligt BDD och innehålla en tydlig beskrivning
+    # ❖ Feature-filen skall kopplas till Selenium Webdriver-kod som utför testandet
+
+  # ❖ Testfallen skall köras med minst en Scenario Outline
+  # ❖ Testfallen skall köras på minst två browsrar
+  # ❖ Skapa minst en privat metod som använder sig av explicit wait
